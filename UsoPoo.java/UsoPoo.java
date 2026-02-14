7d9f5a1d3f2d4244497dc0c39ac1188a29f676ef
Carlos Sanchez Gonzalez <hypnoticdata777@gmail.com>
2026-02-13 21:55:00 -0600

    Complete OOP inheritance and polymorphism homework with Vehiculo hierarchy and main demonstration

--- UsoPoo.java/UsoPoo.java
+++ UsoPoo.java/UsoPoo.java
@@ -64,3 +64,86 @@ public void mostrarInfo() {
     System.out.println("Cilindrada: " + cilindrada + " cc");
 }
 }
+// Clase MotoEstandar
+class MotoEstandar extends Moto {
+private int velocidades;
+public MotoEstandar(String id, String marca, int cilindrada, int velocidades) {
+    super(id, marca, cilindrada);
+    this.velocidades = velocidades;
+}
+
+@Override
+public void mostrarInfo() {
+    System.out.println("=== MOTO ESTANDAR ===");
+    System.out.println("ID: " + id);
+    System.out.println("Marca: " + marca);
+    System.out.println("Cilindrada: " + cilindrada + " cc");
+    System.out.println("Velocidades: " + velocidades);
+}
+}
+// Clase MotoAutomatica
+class MotoAutomatica extends Moto {
+private String tipoTransmision;
+public MotoAutomatica(String id, String marca, int cilindrada, String tipoTransmision) {
+    super(id, marca, cilindrada);
+    this.tipoTransmision = tipoTransmision;
+}
+
+@Override
+public void mostrarInfo() {
+    System.out.println("=== MOTO AUTOMATICA ===");
+    System.out.println("ID: " + id);
+    System.out.println("Marca: " + marca);
+    System.out.println("Cilindrada: " + cilindrada + " cc");
+    System.out.println("Tipo de Transmision: " + tipoTransmision);
+}
+}
+// Clase Avion
+class Avion extends Vehiculo {
+private int motores;
+public Avion(String id, String marca, int motores) {
+    super(id, marca);
+    this.motores = motores;
+}
+
+@Override
+public void mostrarInfo() {
+    System.out.println("=== AVION ===");
+    System.out.println("ID: " + id);
+    System.out.println("Marca: " + marca);
+    System.out.println("Motores: " + motores);
+}
+}
+// Clase principal
+public class UsoPoo {
+public static void main(String[] args) {
+System.out.println("DEMOSTRACION DE POLIMORFISMO Y HERENCIA\n");
+
+// Crear instancias de cada tipo
+    Vehiculo coche = new Coche("C001", "Toyota", 4);
+    Vehiculo trailer = new Trailer("T001", "Kenworth", 25.5);
+    Vehiculo motoEstandar = new MotoEstandar("ME001", "Honda", 600, 6);
+    Vehiculo motoAutomatica = new MotoAutomatica("MA001", "Yamaha", 125, "CVT");
+    Vehiculo avion = new Avion("A001", "Boeing", 2);
+
+    // Demostrar polimorfismo
+    coche.mostrarInfo();
+    System.out.println();
+
+    trailer.mostrarInfo();
+    System.out.println();
+
+    motoEstandar.mostrarInfo();
+    System.out.println();
+
+    motoAutomatica.mostrarInfo();
+    System.out.println();
+
+    avion.mostrarInfo();
+    System.out.println();
+
+    System.out.println("*** POLIMORFISMO DEMOSTRADO ***");
+    System.out.println("Todas las variables son tipo 'Vehiculo' pero ejecutan");
+    System.out.println("el metodo mostrarInfo() de su clase real.");
+}
+}
